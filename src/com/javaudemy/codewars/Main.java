8a686eeb2214df9057c2f90c7889d0223d4acd87
jozwik <juzwik@gmail.com>
2021-07-01 19:40:02 +0200

    add codewars task solution

---
+++ src/com/javaudemy/codewars/Main.java
@@ -0,0 +1,12 @@
+package com.javaudemy.codewars;
+
+public class Main {
+    public static void main(String[] args) {
+//        Solution misc = new Solution();
+//        int result = misc.solution(10);
+//        System.out.println(result);
+        System.out.println(Line.Tickets(new int[] {25, 25, 50}));// => YES
+        System.out.println(Line.Tickets(new int[] {25, 100}));
+        System.out.println(Line.Tickets(new int[] {25, 25, 50, 50, 100}));
+    }
+}
