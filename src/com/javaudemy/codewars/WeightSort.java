6604f09e56125bec58fdd83afa4a38bb53a23ff3
jozwik <juzwik@gmail.com>
2021-07-20 18:54:15 +0200

    add CodeWars files

--- src/com/javaudemy/codewars/WeightSort.java
+++ src/com/javaudemy/codewars/WeightSort.java
@@ -1,24 +1,22 @@
 package com.javaudemy.codewars;
 
-import java.util.TreeMap;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class WeightSort {
 
     public static String orderWeight(String s) {
-        TreeMap<String, String> list = new TreeMap<>();
         String[] arr = s.split("[\\s+]");
-        for (String s1 : arr) {
-            list.put(calculateWeight(s1), s1);
-        }
-        return null;
+        Arrays.sort(arr, Comparator.comparingInt(WeightSort::calculateWeight).thenComparing(s1 -> s1));
+        return String.join(" ", arr);
     }
 
-    public static String calculateWeight(String n) {
+    public static int calculateWeight(String n) {
         int sum = 0;
         for (int i = 0; i < n.length(); i++) {
             sum += Character.getNumericValue(n.charAt(i));
         }
-        return String.valueOf(sum);
+        return sum;
     }
 
     public static void main(String[] args) {
